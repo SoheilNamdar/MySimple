b2b05ec750dc334075cde39fc63c8988982a9fa3
SoheilNamdar <soheilnamdar@yahoo.com>
2020-04-03 08:36:38 +0200

    add spring web

--- src/main/java/com/mkyong/BookRepository.java
+++ src/main/java/com/mkyong/BookRepository.java
@@ -1,10 +1,11 @@
 package com.mkyong;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findByName(String name);
 
